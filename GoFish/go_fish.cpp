4c4e9e541a2849fec94f27e123d3cde3294d0478
Sumedh Zope <szope@utexas.edu>
2019-11-11 20:30:25 -0600

    game .3

--- GoFish/go_fish.cpp
+++ GoFish/go_fish.cpp
@@ -26,91 +26,95 @@ int main( )
 
     cout << p1.getName() <<" has : " << p1.showHand() << endl;
     cout << p2.getName() <<" has : " << p2.showHand() << endl;
+    cout << endl;
 
     Card c1, c2;
     int books = 0;
 
     while(d.size() != 0 || books < 26)
     {
-        c1 = p1.chooseCardFromHand();
-        cout << p1.getName() << " asks - Do you have a " << c1.rankString(c1.getRank()) << endl;
+        if(p1.getHandSize() != 0) {
+            c1 = p1.chooseCardFromHand();
 
-        bool fish = false;
-        if(p2.cardInHand(c1))
-        {
-            cout << p2.getName() << " says - Yes. I have a " << c1.rankString(c1.getRank()) << endl;
-            fish = false;
+            cout << p1.getName() << " asks - Do you have a " << c1.rankString(c1.getRank()) << endl;
+
+            bool fish = false;
+            if (p2.cardInHand(c1)) {
+                cout << p2.getName() << " says - Yes. I have a " << c1.rankString(c1.getRank()) << endl;
+                fish = false;
+            } else {
+                cout << p2.getName() << " says - Go Fish" << endl;
+                fish = true;
+            }
+
+            if (fish) {
+                dealHand(d, p1, 1);
+            }
+            if (!fish) {
+                p2.removeCardFromHand(c1);
+                p1.addCard(c1);
+            }
+
+            if (p1.checkHandForBook(c1, c2)) {
+                cout << p1.getName() << " books the " << c1.getRank() << endl;
+                p1.bookCards(c1, c2);
+                books++;
+            }
+            /*if(p2.checkHandForBook(c1, c2))
+            {
+                cout << p2.getName() << " books the " << c1.getRank() << endl;
+                p2.bookCards(c1, c2);
+                books++;
+            }*/
+
+            cout << endl;
         }
         else
         {
-            cout << p2.getName() << " says - Go Fish" << endl;
-            fish = true;
-        }
-
-        if(fish)
-        {
+            cout << endl;
             dealHand(d, p1, 1);
         }
-        if(!fish)
-        {
-            p2.removeCardFromHand(c1);
-            p1.addCard(c1);
-        }
 
-        if(p1.checkHandForBook(c1, c2))
-        {
-            cout << p1.getName() << " books the " << c1.getRank() << endl;
-            p1.bookCards(c1, c2);
-            books++;
-        }
-        if(p2.checkHandForBook(c1, c2))
-        {
-            cout << p2.getName() << " books the " << c1.getRank() << endl;
-            p2.bookCards(c1, c2);
-            books++;
-        }
+        if(p2.getHandSize() != 0) {
+            c1 = p2.chooseCardFromHand();
+            cout << p2.getName() << " asks - Do you have a " << c1.rankString(c1.getRank()) << endl;
 
-        cout << endl;
+            bool fish = false;
+            if (p2.cardInHand(c1)) {
+                cout << p1.getName() << " says - Yes. I have a " << c1.rankString(c1.getRank()) << endl;
+                fish = false;
+            } else {
+                cout << p1.getName() << " says - Go Fish" << endl;
+                fish = true;
+            }
 
-        c1 = p2.chooseCardFromHand();
-        cout << p2.getName() << " asks - Do you have a " << c1.rankString(c1.getRank()) << endl;
+            if (fish) {
+                dealHand(d, p2, 1);
+            }
+            if (!fish) {
+                p1.removeCardFromHand(c1);
+                p2.addCard(c1);
+            }
 
-        fish = false;
-        if(p2.cardInHand(c1))
-        {
-            cout << p1.getName() << " says - Yes. I have a " << c1.rankString(c1.getRank()) << endl;
-            fish = false;
+            /*if(p1.checkHandForBook(c1, c2))
+            {
+                cout << p1.getName() << " books the " << c1.getRank() << endl;
+                p1.bookCards(c1, c2);
+                books++;
+            }*/
+            if (p2.checkHandForBook(c1, c2)) {
+                cout << p2.getName() << " books the " << c1.getRank() << endl;
+                p2.bookCards(c1, c2);
+                books++;
+            }
+
+            cout << endl;
         }
         else
         {
-            cout << p1.getName() << " says - Go Fish" << endl;
-            fish = true;
-        }
-
-        if(fish)
-        {
+            cout << endl;
             dealHand(d, p2, 1);
         }
-        if(!fish)
-        {
-            p1.removeCardFromHand(c1);
-            p2.addCard(c1);
-        }
-
-        if(p1.checkHandForBook(c1, c2))
-        {
-            cout << p1.getName() << " books the " << c1.getRank() << endl;
-            p1.bookCards(c1, c2);
-            books++;
-        }
-        if(p2.checkHandForBook(c1, c2))
-        {
-            cout << p2.getName() << " books the " << c1.getRank() << endl;
-            p2.bookCards(c1, c2);
-            books++;
-        }
-
-        cout << endl;
     }
 
     if(p1.getBookSize() > p2.getBookSize())
